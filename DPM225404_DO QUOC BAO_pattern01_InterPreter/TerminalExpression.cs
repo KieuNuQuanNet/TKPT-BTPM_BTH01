91d9fc03ef5548ad327f6ccfa9657ecc0ed9fde4
DPM225404 DO QUOC BAO <bao_dpm225404@student.agu.edu.vn>
2025-09-24 14:38:48 +0700

    Add files via upload

---
+++ DPM225404_DO QUOC BAO_pattern01_InterPreter/TerminalExpression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225404_DO_QUOC_BAO_pattern01_InterPreter
+{
+    public class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Called Terminal.Interpret()");
+        }
+    }
+}
---
+++ DPM225404_DO QUOC BAO_pattern01_Proxy/Program.cs
@@ -0,0 +1,14 @@
+﻿using DPM225404_DO_QUOC_BAO_pattern01_Proxy;
+using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        // Create proxy and request a service
+            Proxy proxy = new Proxy();
+            proxy.Request();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
